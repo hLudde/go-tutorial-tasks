df2b9229ed4cc8cd9252a8f9010663284eb33157
Hans Ludvig Flått-Bjørnstad <hansludvig@hotmail.no>
2019-07-01 18:10:38 +0200

    part-8

---
+++ part-8/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+type car struct {
+	posX          float64
+	posY          float64
+	steeringWheel int16
+	topSpeedKmh   float64
+}
+
+func (c *car) hardTurn() int16 {
+	return -c.steeringWheel
+}
+
+var aCar = createCar(100.4, 250.3, 225.0, 5430)
+
+func createCar(posX, posY, topSpeedKmh float64, steeringWheel int16) car {
+	return car{posX, posY, steeringWheel, topSpeedKmh}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<p>values of aCar with type car: posX=%f posY=%f topSpeedKmh=%f steeringWheel=%d</p> <a href=\"/hardTurn\">Do a hard turn</a> <a href=\"/stop\">Stop server</a>", aCar.posX, aCar.posY, aCar.topSpeedKmh, aCar.steeringWheel)
+}
+
+func turnHandler(w http.ResponseWriter, r *http.Request) {
+	aCar.steeringWheel = aCar.hardTurn()
+	indexHandler(w, r)
+}
+
+func stopServer(w http.ResponseWriter, r *http.Request) {
+	os.Exit(0)
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/stop", stopServer)
+	http.HandleFunc("/hardTurn", turnHandler)
+	http.ListenAndServe(":80", nil)
+}
