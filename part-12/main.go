362c17d4f4f4ef7fb3b82fc0c1ac197f7f465e42
Hans Ludvig Flått-Bjørnstad <hansludvig@hotmail.no>
2019-07-02 13:05:56 +0200

    part-12

---
+++ part-12/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+//Sitemapindex to store the parsed xml document
+type Sitemapindex struct {
+	Locations []Location `xml:"sitemap"`
+}
+
+//Location points to the url's in the xml document
+type Location struct {
+	Loc string `xml:"loc"`
+}
+
+func (l Location) String() string {
+	return fmt.Sprintf(l.Loc)
+}
+
+func requestWebsite(url string) *http.Response {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("There was an error fetching the data.")
+		fmt.Println(err.Error())
+		os.Exit(0)
+	}
+	return resp
+}
+
+func getResponseBody(resp *http.Response) []byte {
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("There was an error reading the data.")
+		fmt.Println(err.Error())
+		os.Exit(0)
+	}
+	return bytes
+}
+
+func bytesToString(bytes []byte) string {
+	return string(bytes)
+}
+
+func parseSitemapWebsite(url string) Sitemapindex {
+	var sitemap Sitemapindex
+	resp := requestWebsite(url)
+	bytes := getResponseBody(resp)
+	resp.Body.Close()
+	xml.Unmarshal(bytes, &sitemap)
+	return sitemap
+}
+
+func getWebsiteAsString(url string) string {
+	resp := requestWebsite(url)
+	bytes := getResponseBody(resp)
+	resp.Body.Close()
+	return bytesToString(bytes)
+}
+
+func printSitemapLoop(s Sitemapindex) {
+	for _, Location := range s.Locations {
+		fmt.Printf("%s\n", strings.TrimPrefix(strings.TrimSuffix(Location.String(), "\n"), "\n"))
+	}
+}
+
+func main() {
+	//fmt.Println(getWebsiteAsString("https://www.washingtonpost.com/news-sitemaps/index.xml"))
+	printSitemapLoop(parseSitemapWebsite("https://www.washingtonpost.com/news-sitemaps/index.xml"))
+	os.Exit(0)
+}
