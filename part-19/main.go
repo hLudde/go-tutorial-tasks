b50ebe5c75b1955204fc3100837a96514bd215f6
Hans Ludvig Flått-Bjørnstad <hansludvig@hotmail.no>
2019-07-03 16:24:21 +0200

    part-19

---
+++ part-19/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
+
+func say(text string, n int) {
+	for i := 0; i < n; i++ {
+		fmt.Println(text)
+		time.Sleep(100 * time.Millisecond)
+	}
+	wg.Done()
+}
+
+func main() {
+	wg.Add(1)
+	go say("Hello", 3)
+	wg.Add(1)
+	go say("World!", 5)
+	wg.Wait()
+	wg.Add(1)
+	go say("These messages came after the previous go routines where done executing!", 50)
+	wg.Wait()
+}
